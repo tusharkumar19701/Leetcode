d2c28f87e9ccca5e460f1fc72d6374092ed1c407
Tushar Kumar <tusharkumar19701@gmail.com>
2024-01-29 05:40:21 +0000

    .

---
+++ 102.cpp
@@ -0,0 +1,37 @@
+//Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        if(root == NULL) 
+            return ans;
+        
+        while(1) {
+            int size = q.size();
+            if(size == 0)
+                return ans;
+            vector<int> data;
+            while(size > 0) {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                data.push_back(temp -> val);
+                if(temp -> left != NULL)
+                    q.push(temp -> left);
+
+                if(temp -> right != NULL)
+                    q.push(temp -> right);
+
+                size--;
+            }
+            ans.push_back(data);
+        }
+        return ans;
+   
+    }
+};
